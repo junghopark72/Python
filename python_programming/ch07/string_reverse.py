d18a2628ad0b4271f2bcb3b48832fa308cb719c4
Jungho Park <unipark00@naver.com>
2016-11-12 20:18:22 +0900

    ch07-string_reverse.py

--- python_programming/ch07/string_reverse.py
+++ python_programming/ch07/string_reverse.py
@@ -2,5 +2,13 @@
 
 s = input('영어 문장을 입력하세요.\n')
 
-for c in range(len(s), 0):
-    print(c)
+new_s = str()
+
+for x in range(len(s)-1, -1, -1):
+    new_s += s[x]
+
+print(new_s)
+
+print(s[::-1])
+
+# range([start=0], end, [step=1])
