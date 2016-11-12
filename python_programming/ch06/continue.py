35830ac9214c011e848aaed5155a8f1b58574997
Jungho Park <unipark00@naver.com>
2016-11-12 21:21:42 +0900

    delete ch06 files

--- python_programming/ch06/continue.py
+++
@@ -1,9 +0,0 @@
-signals = 'blue', 'yellow', 'red'
-
-for x in range(len(signals)):
-    print(x, signals[x], '루프 시작!')
-    if signals[x] == 'yellow':
-        continue
-    print(x, signals[x], '루프 종료!')
-
-print('프로그램 종료!!')
--- python_programming/ch06/for_range.py
+++
@@ -1,7 +0,0 @@
-# for_range.py
-
-nest = [[1, 2, 3], [4, 5, 6,], [7, 8, 9]]
-
-for x in range(len(nest)):
-    for y in range(len(nest[x])):
-        print("nest[%d][%d]: %d" % (x, y, nest[x][y]))
